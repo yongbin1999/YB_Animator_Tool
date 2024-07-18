9e042c06a129a5985c68185c48e14df87c2e7a8e
yongbin1999 <47911980+yongbin1999@users.noreply.github.com>
2024-07-18 20:52:18 +0800

    Add files via upload

--- YB_Animator_Tool/Ui/Ui.py
+++ YB_Animator_Tool/Ui/Ui.py
@@ -2,32 +2,10 @@ import bpy
 from ..Operators.Output import KEYS_OT_output
 
 class RENDER_OT_open_keys_render_popup(bpy.types.Operator):
-    """打开关键帧渲染"""
+    """渲染蜡笔关键帧"""
     bl_idname = "render.open_keys_render_popup"
     bl_label = "Crayon Keyframe Rendering"
-    
+
     def execute(self, context):
-        bpy.ops.wm.call_menu(name="UI_MT_keys_render_settings")
+        bpy.ops.keys.output('INVOKE_DEFAULT')
         return {'FINISHED'}
-
-# 窗口界面
-class UI_MT_keys_render_settings(bpy.types.Menu):
-    bl_label = "Keyframe Rendering Settings"
-    bl_idname = "UI_MT_keys_render_settings"
-
-    def draw(self, context):
-        layout = self.layout
-
-        box = layout.box()
-        box.label(text="Basic Settings:")
-        row = box.row()
-
-        # 设置图层标识
-        row.prop(context.scene, "layer_tag", text="LayerTag")
-        # 渲染路径
-        row.prop(context.scene.render, "filepath")
-        # 执行渲染操作
-        row = layout.row()
-        row.scale_y = 3
-        row.operator("keys.output", icon='EXPORT', text="Render Crayon Keyframes")
-
--- YB_Animator_Tool/__init__.py
+++ YB_Animator_Tool/__init__.py
@@ -1,38 +1,68 @@
 import bpy
-from .Ui.Ui import RENDER_OT_open_keys_render_popup, UI_MT_keys_render_settings
+from .Ui.Ui import RENDER_OT_open_keys_render_popup
 from .Operators.Output import KEYS_OT_output
 
 bl_info = {
     "name": "YB_Animator_Tool",
     "author": "YB_",
-    "description": "Tool for Blender animation",
+    "description": "Tool for Blender animation,export grease pencil layers as keyframes and CSV",
     "blender": (4, 0, 0),
-    "version": (1, 1, 0),
+    "version": (1, 1, 2),
     "location": "Crayon Keyframe Rendering is in the render menu at the top",
     "warning": "",
     "category": "YB_Animator",
     "doc_url": "https://github.com/yongbin1999/YB_Animator_Tool"
 }
 
-def render_menu_func(self, context):
-    self.layout.operator(RENDER_OT_open_keys_render_popup.bl_idname, icon='RENDER_STILL')
 
-# 注册
-def register():
-    bpy.utils.register_class(KEYS_OT_output)
-    bpy.utils.register_class(RENDER_OT_open_keys_render_popup)
-    bpy.utils.register_class(UI_MT_keys_render_settings)
-    bpy.types.TOPBAR_MT_render.append(render_menu_func)
-    bpy.types.Scene.layer_tag = bpy.props.StringProperty(
+class YBAnimatorPreferences(bpy.types.AddonPreferences):
+    bl_idname = __name__
+
+    layer_tag: bpy.props.StringProperty(
         name="Layer Tag",
-        default="动画"
+        default="Keys",
+        update=lambda self, context: update_layer_tag(context)
     )
 
+    def draw(self, context):
+        layout = self.layout
+        layout.prop(self, "layer_tag")
+
+# Update callback function
+
+
+def update_layer_tag(context):
+    prefs = bpy.context.preferences.addons[__name__].preferences
+    bpy.types.Scene.layer_tag = prefs.layer_tag
+
+
+def render_menu_func(self, context):
+    self.layout.operator(
+        RENDER_OT_open_keys_render_popup.bl_idname, icon='RENDER_STILL')
+
+# Registration
+
+
+def register():
+    bpy.utils.register_class(YBAnimatorPreferences)
+    bpy.utils.register_class(KEYS_OT_output)
+    bpy.utils.register_class(RENDER_OT_open_keys_render_popup)
+    bpy.types.TOPBAR_MT_render.append(render_menu_func)
+
+    addon_prefs = bpy.context.preferences.addons[__name__].preferences
+    bpy.types.Scene.layer_tag = bpy.props.StringProperty(
+        name="Layer Tag",
+        default=addon_prefs.layer_tag
+    )
+
+
 def unregister():
+    bpy.utils.unregister_class(YBAnimatorPreferences)
     bpy.utils.unregister_class(KEYS_OT_output)
     bpy.utils.unregister_class(RENDER_OT_open_keys_render_popup)
-    bpy.utils.unregister_class(UI_MT_keys_render_settings)
     bpy.types.TOPBAR_MT_render.remove(render_menu_func)
     del bpy.types.Scene.layer_tag
 
 
+if __name__ == "__main__":
+    register()
